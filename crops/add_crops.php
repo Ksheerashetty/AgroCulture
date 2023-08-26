396429aa8da5fa3511bc46a3ebdda3e46a4a695e
Shameer Abdul Kader <shameerak896@gmail.com>
2023-08-26 14:47:00 +0530

    fixed the error caused

--- crops/add_crops.php
+++ crops/add_crops.php
@@ -4,8 +4,9 @@ include '../db.php';
 
 $crop=$_POST['crops'];
 $quantity=$_POST['quantity'];
+$id=$_SESSION['id'];
 
-$q="insert into crop (fid, cname, quantity)values('$_SESSION['id']','$crop','$quantity')";
+$q="insert into crop (fid, cname, quantity)values('$id','$crop','$quantity')";
 if(mysqli_query($con,$q))
 {
     header("Location:../crops/crop.html");

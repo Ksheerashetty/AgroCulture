1dcb238633ecc05eb53b202b5d04fc5ebf75f657
Shameer Abdul Kader <shameerak896@gmail.com>
2023-08-26 14:43:24 +0530

    changed few parameters

--- crops/add_crops.php
+++ crops/add_crops.php
@@ -1,11 +1,11 @@
 <?php
-
+session_start();
 include '../db.php';
 
 $crop=$_POST['crops'];
 $quantity=$_POST['quantity'];
 
-$q="insert into crop (fid, cname, quantity)values('121','$crop','$quantity')";
+$q="insert into crop (fid, cname, quantity)values('$_SESSION['id']','$crop','$quantity')";
 if(mysqli_query($con,$q))
 {
     header("Location:../crops/crop.html");

f2651b0eb4678822834b263e3f0cf14a0a5e4062
Shameer Abdul Kader <shameerak896@gmail.com>
2023-08-26 14:00:33 +0530

    created login php script for login

---
+++ log.php
@@ -0,0 +1,34 @@
+<?php
+
+include 'db.php';
+session_start();
+
+$name=$_POST['name'];
+$pass=$_POST['pass'];
+
+$q="select * from farmer where fname='$name' AND password='$pass'";
+
+if($r=mysqli_query($con,$q)){
+    $n=mysqli_num_rows($r);
+}
+else
+{
+    $n=0;
+}
+
+$array=mysqli_fetch_assoc($r);
+
+if($n==1)
+{
+    $_SESSION['id']=$array['fid'];
+    header("Location:sales/sales.html");
+    exit();
+}
+else
+{
+    $_SESSION['message']="error";
+    header("Location:login.php");
+    exit();
+}
+
+?>

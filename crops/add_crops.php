e45cae02d321476f013ed2c18ef33f80af58e3c2
Shameer Abdul Kader <shameerak896@gmail.com>
2023-08-27 19:38:30 +0530

    Added few changes for the validation

--- crops/add_crops.php
+++ crops/add_crops.php
@@ -6,10 +6,27 @@ $crop=$_POST['crops'];
 $quantity=$_POST['quantity'];
 $id=$_SESSION['id'];
 
+$q1="select * from crop where cname='$crop'";
+if($r=mysqli_query($con,$q1)){
+    $n=mysqli_num_rows($r);
+}
+else
+{
+    $n=0;
+}
+if(n==0)
+{}
 $q="insert into crop (fid, cname, quantity)values('$id','$crop','$quantity')";
 if(mysqli_query($con,$q))
 {
     header("Location:../crops/crop.php");
+    exit();
+}
+}
+else
+{
+    $_SESSION['message']="error";
+    header("Location:../crops.crop.php");
     exit();
 }
 
